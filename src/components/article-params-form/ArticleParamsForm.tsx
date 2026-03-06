c3cecd47228ba997d0b9649e9c7b1a455ea0bca0
Said Basidov <saidbasidov@gmail.com>
2026-03-06 11:52:31 +0500

    First Review

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,110 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import React, { useEffect, useRef, useState } from 'react';
+import clsx from 'clsx';
+import { ArticleStateType, backgroundColors, contentWidthArr, defaultArticleState, fontColors, fontFamilyOptions, fontSizeOptions } from 'src/constants/articleProps';
+import { Select } from 'src/ui/select';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Separator } from 'src/ui/separator';
+import { Text } from 'src/ui/text';
+import { flushSync } from 'react-dom';
 
-export const ArticleParamsForm = () => {
+type TFormProps = {
+	onApply: (newState: ArticleStateType) => void,
+	onReset: () => void
+}
+
+export const ArticleParamsForm = (props: TFormProps) => {
+	const {onApply, onReset} = props
+
+	const [isOpen, setIsOpen] = useState(false);
+	const [formState, setFormState] = useState<ArticleStateType>(defaultArticleState);
+
+	const asideRef = useRef<HTMLElement | null>(null);
+	const arrowBtnRef = useRef<HTMLDivElement | null>(null);
+
+	useEffect(() => {
+		if (!isOpen) return;
+
+		function click(event: MouseEvent) {
+			const target = event.target as Node;
+
+			if (
+				asideRef.current?.contains(target) ||
+				arrowBtnRef.current?.contains(target)
+			) {
+				return;
+			}
+
+			setIsOpen(false);
+		}
+
+		document.addEventListener('mousedown', click);
+
+		return () => {
+			document.removeEventListener('mousedown', click);
+		};
+	}, [isOpen]);
+
+	const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		onApply(formState)
+		setIsOpen(false)
+	}
+
+	const handleReset = (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		setFormState(defaultArticleState)
+		onReset()
+	}
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
-				<form className={styles.form}>
+			<div ref={arrowBtnRef}>
+				<ArrowButton
+					isOpen={isOpen}
+					onClick={() => setIsOpen((prev) => !prev)}
+				/>
+			</div>
+			<aside
+				ref={asideRef}
+				className={clsx(styles.container, isOpen && styles.container_open)}>
+				<form className={styles.form} onSubmit={handleSubmit} onReset={handleReset}>
+					<Text
+						size={31}
+						weight={800}
+						uppercase
+					>
+						Задайте параметры
+					</Text>
+					<Select
+						selected={formState.fontFamilyOption}
+						options={fontFamilyOptions}
+						onChange={(option) => setFormState(prev => ({ ...prev, fontFamilyOption: option }))}
+						title='Шрифт' />
+					<RadioGroup
+						name='fontSize'
+						selected={formState.fontSizeOption}
+						options={fontSizeOptions}
+						onChange={(option) => setFormState(prev => ({ ...prev, fontSizeOption: option }))}
+						title='Размер Шрифта' />
+					<Select
+						selected={formState.fontColor}
+						options={fontColors}
+						onChange={(option) => setFormState(prev => ({ ...prev, fontColor: option }))}
+						title='Цвет Шрифта' />
+					<Separator />
+					<Select
+						selected={formState.backgroundColor}
+						options={backgroundColors}
+						onChange={(option) => setFormState(prev => ({ ...prev, backgroundColor: option }))}
+						title='Цвет Фона' />
+					<Select
+						selected={formState.contentWidth}
+						options={contentWidthArr}
+						onChange={(option) => setFormState(prev => ({ ...prev, contentWidth: option }))}
+						title='Ширина Контента' />
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
--- src/ui/select/Select.tsx
+++ src/ui/select/Select.tsx
@@ -67,7 +67,7 @@ export const Select = (props: SelectProps) => {
 						styles.placeholder,
 						(styles as Record<string, string>)[optionClassName]
 					)}
-					data-status={status}
+					// data-status={status}
 					data-selected={!!selected?.value}
 					onClick={handlePlaceHolderClick}
 					role='button'
